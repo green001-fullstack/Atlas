d30b28e677d3872ac379a270951509e3e8f709d6
Oladimeji Emmanuel <oladimejiemmanuel2015@gmail.com>
2026-08-01 01:55:38 +0100

    feat(domain): implement PhoneNumber value object

--- backend/internal/applicant/domain/valueobjects/errors.go
+++ backend/internal/applicant/domain/valueobjects/errors.go
@@ -3,3 +3,4 @@ package valueobjects
 import "errors"
 
 var ErrInvalidEmailAddress = errors.New("invalid email address")
+var ErrInvalidPhoneNumber = errors.New("invalid phone number")
--- backend/internal/applicant/domain/valueobjects/phone_number.go
+++ backend/internal/applicant/domain/valueobjects/phone_number.go
@@ -1,25 +1,69 @@
 package valueobjects
 
-import "strings"
+import (
+	"regexp"
+	"strings"
+)
 
-func normalizePhoneNumber(text string) string{
-	newText := strings.ReplaceAll(text, " ", "")
-	finalText := strings.ReplaceAll(newText, "-", "")
-	return finalText
+var digitsOnly = regexp.MustCompile(`[^0-9]`)
+
+func normalizePhoneNumber(text string) (string, error) {
+
+	finalNumber := digitsOnly.ReplaceAllString(text, "")
+
+	var transformedNumber string
+	if len(finalNumber) == 11 {
+		transformedNumber = "+234" + finalNumber[1:]
+		return transformedNumber, nil
+	}
+
+	if len(finalNumber) == 13 {
+		transformedNumber = "+" + finalNumber
+		return transformedNumber, nil
+	}
+
+	return "", ErrInvalidPhoneNumber
 }
 
-func validatePhoneNumber(text string) error{
-	return nil
+func validatePhoneNumber(text string) error {
+
+	if len(text) != 14 {
+		return ErrInvalidPhoneNumber
+	}
+
+	validPrefixes := []string{
+		"+23470",
+		"+23480",
+		"+23481",
+		"+23490",
+		"+23491",
+	}
+
+		for _, prefix := range validPrefixes {
+			if strings.HasPrefix(text, prefix) {
+				return nil
+			}
+		}
+
+	return ErrInvalidPhoneNumber
 }
 
-type PhoneNumber struct{
+type PhoneNumber struct {
 	value string
 }
 
-func NewPhoneNumber( value string) (PhoneNumber, error){
-	return PhoneNumber{ value: value}, nil
+func NewPhoneNumber(value string) (PhoneNumber, error) {
+	normalizedPhoneNumber, err := normalizePhoneNumber(value)
+	if err != nil {
+		return PhoneNumber{}, err
+	}
+	err = validatePhoneNumber(normalizedPhoneNumber)
+	if err != nil {
+		return PhoneNumber{}, err
+	}
+	return PhoneNumber{value: normalizedPhoneNumber}, nil
 }
 
-func(p PhoneNumber) String() string{
+func (p PhoneNumber) String() string {
 	return p.value
 }
